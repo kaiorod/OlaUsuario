c1b3ce9c8535d5708358be1f2e9835dff1050704
kaiorod <caiorodrigo2775@gmail.com>
2021-08-20 22:48:11 -0300

    finaliza OlaUsuario

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OlaUsuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            Console.Clear();
+            Console.Beep();
+            Console.Write("Digite seu nome : ");
+            nome = Console.ReadLine();
+            
+            Console.Beep();
+            Console.Write("Olá, ");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.Write($"{nome}");
+            Console.ResetColor();
+            Console.Write("!");
+            
+        }
+    }
+}
